d07036aa19d6605041e413eabe843522fad3283d
giovanninavar313 <giomarasco12@gmail.com>
2024-10-28 16:34:59 +0100

    Add files via upload
    Con questo progetto scriverai le parole che vuoi scrivere per quante volte vuoi tu, ricordati che il massimo è 1000000.

---
+++ moltiplicatoreparole.py
@@ -0,0 +1,8 @@
+def molti():  
+    parola = str(input("Cosa vuoi scrivere?\n ")) 
+    numero = int(input("Per quante volte?\n ")) 
+    print( parola * numero )
+  
+i = 1
+while i<2: 
+    molti() 
